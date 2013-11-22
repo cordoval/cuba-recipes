2303fb5b7d641fbb1c22bae673748c7681811b65
Francesco Rodriguez <frodsan@me.com>
2013-11-22 04:51:52 -0500

    Remove status_code helper

--- test/01_hello_world.rb
+++ test/01_hello_world.rb
@@ -5,7 +5,7 @@ scope do
   test 'hello world' do
     get '/'
 
-    assert_equal status_code(:ok), last_response.status
+    assert_equal 200, last_response.status
     assert_equal 'Hello Cuba!', last_response.body
   end
 end
--- test/helper.rb
+++ test/helper.rb
@@ -4,9 +4,3 @@ def require_recipe name
   dir = ENV['SOLUTION'] ? 'solutions' : 'recipes'
   require_relative "../#{dir}/#{name}/app"
 end
-
-class Cutest::Scope
-  def status_code code
-    Rack::Utils.status_code code
-  end
-end
